040bc2cfa00bb5d7c0d8819c6d642477294c0d9f
Danny Garcia <dannygarcia.me@gmail.com>
2016-07-05 21:46:36 -0400

    add show page for a meal

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,6 +3,10 @@ class MealsController < ApplicationController
     @meals = Meal.all
   end
 
+  def show
+    @meal = Meal.find(params[:id])
+  end
+
   def new
     @meal = Meal.new
     2.times { @meal.items.build  }
